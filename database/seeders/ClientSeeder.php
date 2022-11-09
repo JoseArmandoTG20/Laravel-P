ce6f7d8482a24c850d7802fba12193d10140503c
JoseArmandoTG20 <josearman2_19@alu.uabcs.mx>
2022-11-08 21:32:24 -0700

    Seeders

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Guesser\Name;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('clients')->insert([
+            'name'=> 'Armando',
+            'lastname'=> 'Torres',
+            'email'=> 'armandopromx@gmail.com',
+            'phone_number'=> '6121356944',
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+    */
+    public function run()
+    {
+        $user = new User();
+        $user -> name = 'Armando ';
+        $user -> email = 'armandopromx@gmail.com';
+        $user -> password = 'armando12345';
+        $user -> save();
+
+        $user = new User();
+        $user -> name = 'Dinah';
+        $user -> email = 'dinahG@gmail.com';
+        $user -> password = '123456789';
+        $user -> save();
+    }
+}
+

e66100e92ecde579bbc719ced52ae097080dcf98
JoseArmandoTG20 <josearman2_19@alu.uabcs.mx>
2022-11-10 19:05:40 -0700

    Plantilla con ayuda de include, yield y extends

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-class ClientSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -19,14 +19,14 @@ class ClientSeeder extends Seeder
         $user -> name = 'Armando ';
         $user -> lastname = 'Torres';
         $user -> email = 'armandopromx@gmail.com';
-        $user -> password = 'armando12345';
+        $user -> password = bcrypt('armando12345');
         $user -> save();
 
         $user = new User();
         $user -> name = 'Dinah';
         $user -> lastname = 'Carpenter';
         $user -> email = 'dinahG@gmail.com';
-        $user -> password = '123456789';
+        $user -> password = bcrypt('123456789');
         $user -> save();
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola {{ Auth::user()->name}}</h1>
+@extends('layouts.app')
 
-    <a href="{{url('users')}}">Ir a Usuarios.</a>
+@section('head')
+    <-- Hello, Soy una Prueba -->
+@endsection
 
-    <form method="post" action="{{url('logout')}}">
-        @csrf
+@section('contenido')
+<h1>Hola {{ Auth::user()->name}}</h1>
 
-        <button type="submit">
-            Salir
-        </button>
-    </form>
-</body>
-</html>
+<a href="{{url('users')}}">Ir a Usuarios.</a>
+
+<form method="post" action="{{url('logout')}}">
+    @csrf
+
+    <button type="submit">
+        Salir
+    </button>
+</form>
+
+@endsection
